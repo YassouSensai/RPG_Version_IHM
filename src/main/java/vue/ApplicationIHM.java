900eec2cd4cd187f0eb3f4a0724e402fdcd577cb
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-05-21 22:52:10 +0200

    maj

--- src/main/java/vue/ApplicationIHM.java
+++ src/main/java/vue/ApplicationIHM.java
@@ -1,4 +1,30 @@
 package vue;
 
-public class ApplicationIHM {
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class ApplicationIHM extends Application {
+
+    private Parent root;
+
+    public void start(Stage stage) {
+        HBoxRoot root = new HBoxRoot();
+        Scene scene = new Scene(root, 700, 400);
+        File feuilleCSS = new File("css"+File.separator+"feuilleStyle.css");
+
+        scene.getStylesheets().add(feuilleCSS.toURI().toString());
+
+
+        stage.setScene(scene);
+        stage.setTitle("Role Playing Game");
+        stage.show();
+    }
+    public static void main (String [] args ){
+        Application.launch(args);
+    }
 }
---
+++ src/main/java/vue/HBoxRoot.java
@@ -0,0 +1,9 @@
+package vue;
+
+import javafx.scene.layout.HBox;
+
+public class HBoxRoot extends HBox {
+    public HBoxRoot() {
+        super();
+    }
+}
