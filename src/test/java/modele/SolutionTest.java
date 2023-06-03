547ec56a191c10bbbb75a97644a03e3f56d9c76e
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-06-03 21:55:39 +0200

    finalisation des tests de la classe Solution

--- src/test/java/modele/SolutionTest.java
+++ src/test/java/modele/SolutionTest.java
@@ -2,17 +2,84 @@ package modele;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class SolutionTest {
 
+
+    /**
+     * Methode de test pour la methode solutionEfficace de la classe Solution.
+     * Cette methode va verifier ;
+     *  - si la quete finale est bien presente dans la solution.
+     *  - si la taille de la solution est bien inferieure ou egale au nombre de solutions.
+     */
     @Test
     void solutionEfficace() {
-        System.out.println("Test de la methode solutionefficace()");
+        System.out.println("Test de la methode solutionEfficace");
+
+        File[] tabFichierScenario = {new File("scenarios"+File.separator+"scenario_0.txt"),
+                new File("scenarios"+File.separator+"scenario_1.txt"),
+                new File("scenarios"+File.separator+"scenario_2.txt"),
+                new File("scenarios"+File.separator+"scenario_3.txt"),
+                new File("scenarios"+File.separator+"scenario_4.txt")};
+        Scenario[] tabScenario = new Scenario[5];
+        Solution[] tabSolution = new Solution[5];
+
+        for (int i = 0; i < tabFichierScenario.length; i++) {
+            System.out.println("\n\nScenario "+i);
+            tabScenario[i] = new LectureFichierTexte().lecture(tabFichierScenario[i]);
+            tabSolution[i] = new Solution(tabScenario[i], 0, 0);
+            ArrayList<Quete> solution = tabSolution[i].solutionEfficace();
+
+            assertEquals(solution.get(solution.size()-1),tabScenario[i].getListeQuetes().get(tabScenario[i].getListeQuetes().size()-1));
+            System.out.println("La quete finale a bien été réalisée !");
+
+            assertTrue(solution.size() <= tabScenario[i].getListeQuetes().size());
+            System.out.println("La taille de la solution est bien cohérente avec le nombre de quetes disponibles !");
+        }
     }
 
+
+    /**
+     * Methode de test pour la methode solutionEfficace de la classe Solution.
+     * Cette methode va verifier ;
+     *  - si la quete finale est bien presente dans la solution.
+     *  - si la taille de la solution est bien egale au nombre de solutions.
+     *  - si toutes les quetes ont bien ete effectuee.
+     */
     @Test
     void solutionExhaustive() {
-        System.out.println("Test de la methode solutionExhaustive()");
-    }
+        System.out.println("Test de la methode solutionExhaustive");
 
+        File[] tabFichierScenario = {new File("scenarios"+File.separator+"scenario_0.txt"),
+                                     new File("scenarios"+File.separator+"scenario_1.txt"),
+                                     new File("scenarios"+File.separator+"scenario_2.txt"),
+                                     new File("scenarios"+File.separator+"scenario_3.txt"),
+                                     new File("scenarios"+File.separator+"scenario_4.txt")};
+        Scenario[] tabScenario = new Scenario[5];
+        Solution[] tabSolution = new Solution[5];
+
+        for (int i = 0; i < tabFichierScenario.length; i++) {
+            System.out.println("\n\nScenario "+i);
+            tabScenario[i] = new LectureFichierTexte().lecture(tabFichierScenario[i]);
+            tabSolution[i] = new Solution(tabScenario[i], 0, 0);
+            ArrayList<Quete> solution = tabSolution[i].solutionExhaustive();
+
+            assertEquals(solution.get(solution.size()-1),tabScenario[i].getListeQuetes().get(tabScenario[i].getListeQuetes().size()-1));
+            System.out.println("La quete finale a bien été réalisée !");
+
+            assertEquals(solution.size(), tabScenario[i].getListeQuetes().size());
+            System.out.println("La taille de la solution est bien cohérente avec le nombre de quetes disponibles !");
+
+            for (Quete quete : tabScenario[i].getListeQuetes()){
+                assertTrue(solution.contains(quete));
+            }
+            System.out.println("Toutes les quetes ont été réalisée !");
+        }
+    }
 }
